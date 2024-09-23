8591848fde41bf8a0f038b6c4b145dd737269b51
VarghesekpQa <infovarghese94@gmail.com>
2024-09-23 18:52:29 +0530

    modified config,product section file

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,9 +17,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1 : 1,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : 3,
+  workers: process.env.CI ? 4 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -28,7 +28,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'off',
   },
 
   /* Configure projects for major browsers */
--- sections/product.section.ts
+++ sections/product.section.ts
@@ -193,7 +193,6 @@ export default class ProductSection {
             await this.page.locator(ps.firstProduct).click()
             await this.page.waitForTimeout(2000)
             for (let j = 1; j < productNameArr.length; j++) {
-              await this.page.focus(ps.productSelect)
               await this.page.fill(ps.productSelect, '')
               await this.page.fill(ps.productSelect, productNameArr[j])
               await this.page.press(ps.productSelect, 'Tab')
